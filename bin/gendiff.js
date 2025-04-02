399438571e3ca7b250045d430f5e7e7f1cd3d411
Egor Ryzhov <ryzhov.egor@gmail.com>
2025-04-02 13:19:16 +0000

    gendiff.js updated

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,36 @@
 #!/usr/bin/env node
-import { Command } from "commander";
+import { Command } from 'commander';
+import path from 'path';
+import fs from 'fs';
 
 const program = new Command();
 
-program
+const gendiff = program
+    .argument('<filepath1>')
+    .argument('<filepath2>')
     .description(`Compares two configuration files and shows a difference.`)
-    .option('-V, --version', 'output the version number');
+    .option('-V, --version', 'output the version number')
+    .option('-f, --format [type]', 'output format');
 
-program.parse();
+gendiff
+    .command('generate')
+    .description('generate template for project')
+    .action(() => {
+        const srcDir = path.join(process.cwd(), 'src');
+        const indexFile = path.join(srcDir, 'index.js');
+        const parserFile = path.join(srcDir, 'parser.js');
+
+        if (!fs.existsSync(srcDir)) {
+            fs.mkdirSync(srcDir);
+        }
+
+        const parserContent = 'export default () => {\n     console.log("parser");\n}';
+        fs.writeFileSync(parserFile, parserContent);
+
+        const indexContent = 'import parser from "./parser.js";\n\nexport default () => {\n console.log("gendiff");\n  parser();\n}';
+        fs.writeFileSync(indexFile, indexContent);
+
+        console.log('Project structure generated successfully');
+    });
+
+gendiff.parse();
